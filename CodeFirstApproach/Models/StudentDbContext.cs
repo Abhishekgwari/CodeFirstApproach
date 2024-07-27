72e5187e147a788b587ecaed1d4467e45d1853ad
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-27 23:52:05 +0530

    Add project files.

---
+++ CodeFirstApproach/Models/StudentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Models
+{
+    public class StudentDbContext:DbContext                // inherit from DbContext
+    {
+
+        public StudentDbContext(DbContextOptions options) : base(options)    // 
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }                         // Db set property  
+                                                        // it represents table of our database   table name will be Students
+    }
+}
